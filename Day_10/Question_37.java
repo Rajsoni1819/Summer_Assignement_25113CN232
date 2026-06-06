1d9db9d999946423fdeac1243133bc685848e8a7
Rajsoni1819 <rajsoni19531270@gmail.com>
2026-06-06 12:27:09 +0530

    Added Question 40 of Day 10

--- Day_10/Question_37.java
+++ Day_10/Question_37.java
@@ -12,11 +12,11 @@ import java.util.Scanner;
 public class Question_37 {
     public static void main(String[] args){
          for (int i = 1; i <= 5; i++) {
-            // Print spaces
+            
             for (int j = 1; j <= 5 - i; j++) {
                 System.out.print(" ");
             }
-            // Print stars
+            
             for (int k = 1; k <= (2 * i - 1); k++) {
                 System.out.print("*");
             }
--- Day_10/Question_39.java
+++ Day_10/Question_39.java
@@ -11,15 +11,15 @@ public class Question_39 {
     public static void main(String[] args) {
         int rows = 5;
         for (int i = 1; i <= rows; i++) {
-            // Print spaces
+           
             for (int j = 1; j <= rows - i; j++) {
                 System.out.print(" ");
             }
-            // Print ascending numbers
+           
             for (int k = 1; k <= i; k++) {
                 System.out.print(k);
             }
-            // Print descending numbers
+           
             for (int l = i - 1; l >= 1; l--) {
                 System.out.print(l);
             }
